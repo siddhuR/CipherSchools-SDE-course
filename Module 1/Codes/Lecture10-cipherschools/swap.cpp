47dbcc641c093b2582efa774abfbd57cbc4820c0
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-06-17 10:30:03 +0530

    swap.cpp

--- Module 1/Codes/Lecture10-cipherschools/swap.cpp
+++ Module 1/Codes/Lecture10-cipherschools/swap.cpp
@@ -18,6 +18,32 @@ int sum(int a, int b)
     return c;
 }
 
+// void swap(int a, int b)
+void swap(int &a, int &b)
+{
+    int c;
+
+    c=a;
+    a=b;
+    b=c;
+
+    cout<<"The values of a and b after swapping inside a function are: "<<a<<" "<<b<<endl;
+
+    return;
+}
+
+int fun(int &a, int b)
+{
+    a= 99999;
+    b=99999;
+    
+    cout<<"The values of a and b after swapping inside fun function are: "<<a<<" "<<b<<endl;
+
+    
+}
+
+
+
 int main()
 {
     PrintHI();
@@ -28,13 +54,24 @@ int main()
     a=4;
     b=6;
 
-    c=75;   //the value of c over here in the main function is 75
+    cout<<"The values of a and b before passing into the fun function are: "<<a<<" "<<b<<endl;
 
-    int d;
-    d = sum(a,b);
+
+    //c=75;   //the value of c over here in the main function is 75
+
+    //int d;
+    //d = sum(a,b);
     
     // cout<<"The sum is equal to "<<d<<" "<<"The value of a is "<<a;   "
-    cout<<"The value of c is equal to "<<c<<endl;
+    //cout<<"The value of c is equal to "<<c<<endl;
+    
+    //swap(a,b);
+    //cout<<"The values of a and b after swapping outside the function are: "<<a<<" "<<b<<endl;
+    
+    fun(a,b);
+    cout<<"The values of a and b after passing to the fun function are: "<<a<<" "<<b<<endl;
+
 
     return 0;
 }
+
