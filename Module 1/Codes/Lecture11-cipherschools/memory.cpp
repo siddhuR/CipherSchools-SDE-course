942153ffa38d66ed433cda3295f0a864a3d7ad8c
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-06-19 11:27:11 +0530

    Add files via upload

---
+++ Module 1/Codes/Lecture11-cipherschools/memory.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long int a;
+    int b;
+    cout<<"The size of int is :"<<sizeof(b)<<" The size of long long int is: "<<sizeof(a);
+    cout<<"The address of a is "<<&a<<"The address of b is "<<&b;
+
+    return 0;
+}
