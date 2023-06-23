5f9a2b78ce6d146b43be59bb1dffcfb810ba4647
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-06-23 12:17:48 +0530

    Add files via upload

---
+++ Module 1/Codes/Lecture19-cipherschools/insertion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"input an array of elements which you have to sort, let us say 6 elements: ";
+
+    int i,j;
+    int a[6];
+
+    for(i=0;i<6;i++) cin>>a[i];
+
+    int key;
+
+    for(j=1;j<6;j++)
+    {
+    
+    key = a[j]; // we are storing the element which is to be inserted
+    i = j-1;
+
+    while(a[i] > key && i>=0)
+    {
+        a[i+1]= a[i];
+        i--;
+    }
+    a[i+1] = key;
+
+    }
+
+    for(i=0;i<6;i++) cout<<a[i];
+
+
+    
+}
