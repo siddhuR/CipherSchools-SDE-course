8a65df4f0ebfe1e7a4ae2ea6f6f5b516f39a8d85
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-07-05 18:23:38 +0530

    Add files via upload

---
+++ Module 1/Codes/Lecture25-cipherschools/oops.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// We achieve this using something called a "class"
+// A class is a user defined datatype which can be customized however we want 
+
+class student
+{
+    public:
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+};
+
+class Car
+{
+    public:
+    
+    string car_name;
+    string tyre_name;
+    int number_of_tyres;
+    int Number_of_seats;
+    
+};
+
+int main()
+{
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+    
+}
