59ef7c84b3d68d890422e135a8cce0a14e98fed0
Routhu Siddhartha <routhusiddhartha@gmail.com>
2023-08-10 19:56:01 +0530

    understanding JSX

--- Module 3-Mern stack/ReactJS/first-app/src/App.js
+++ Module 3-Mern stack/ReactJS/first-app/src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const NAME="WORLD"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1 style={{
+          color: "red",
+          textAlign: "center",
+        }}> Hello {NAME}...!</h1>
+      </div>
+      <div>
+        <p>This is {NAME}</p>
+      </div>
+    </>
+
   );
+  
 }
 
 export default App;
