5f9a67bd41c1f81e10d22b20604dc58e5665d880
Routhu Siddhartha <routhusiddhartha@gmail.com>
2023-08-11 20:00:25 +0530

    Learning React components

--- Module 3-Mern stack/ReactJS/first-app/src/App.js
+++ Module 3-Mern stack/ReactJS/first-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  const NAME="WORLD"
-  return (
-    <>
+class App extends Component {
+
+  name = "Routhu_Siddhartha";
+  render() {
+    return (
+      <>
       <div>
-        <h1 style={{
-          color: "red",
-          textAlign: "center",
-        }}> Hello {NAME}...!</h1>
+        <h1>This is {this.name}</h1>
       </div>
       <div>
-        <p>This is {NAME}</p>
+        <p>
+          This is paragraph and name is : {this.name}
+        </p>
       </div>
-    </>
-
-  );
-  
+      </>
+    )
+  }
 }
+
+
+
+// function App() {
+//   const NAME="WORLD"
+//   return (
+//     <>
+//       <div>
+//         <h1 style={{
+//           color: "red",
+//           textAlign: "center",
+//         }}> Hello {NAME}...!</h1>
+//       </div>
+//       <div>
+//         <p>This is {NAME}</p>
+//       </div>
+//     </>
+
+//   );
+  
+// }
 
 export default App;
