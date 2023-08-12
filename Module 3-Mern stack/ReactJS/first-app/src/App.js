90caaaef52181ee77405d6780d160bb51224f0a1
Routhu Siddhartha <routhusiddhartha@gmail.com>
2023-08-12 23:34:09 +0530

    ToDoScreen implementation

--- Module 3-Mern stack/ReactJS/first-app/src/App.js
+++ Module 3-Mern stack/ReactJS/first-app/src/App.js
@@ -1,45 +1,10 @@
-//import logo from './logo.svg';
-import './App.css';
-import {Component} from 'react';
+import { Component } from "react";
+import ToDoScreen from "./screens/ToDoScreen";
 
 class App extends Component {
-
-  name = "Routhu_Siddhartha";
-  render() {
-    return (
-      <>
-      <div>
-        <h1>This is {this.name}</h1>
-      </div>
-      <div>
-        <p>
-          This is paragraph and name is : {this.name}
-        </p>
-      </div>
-      </>
-    )
-  }
+    render() {
+        return <ToDoScreen />
+    }
 }
-
-
-
-// function App() {
-//   const NAME="WORLD"
-//   return (
-//     <>
-//       <div>
-//         <h1 style={{
-//           color: "red",
-//           textAlign: "center",
-//         }}> Hello {NAME}...!</h1>
-//       </div>
-//       <div>
-//         <p>This is {NAME}</p>
-//       </div>
-//     </>
-
-//   );
-  
-// }
 
 export default App;
