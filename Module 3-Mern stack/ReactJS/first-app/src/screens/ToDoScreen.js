9fdc7b8672b5b652dfb223f6cdb1e5fda04b18e5
siddhuR <routhusiddhartha@gmail.com>
2023-08-29 17:29:17 +0530

    adding ToDoScreen react

--- Module 3-Mern stack/ReactJS/first-app/src/screens/ToDoScreen.js
+++ Module 3-Mern stack/ReactJS/first-app/src/screens/ToDoScreen.js
@@ -1,8 +1,24 @@
 import { Component } from "react";
 
 class ToDoScreen extends Component {
+
+    state = {
+        taskCount: 0,
+    };
     render() {
-        return <h1 className="ui heading center">To Do List </h1>;
+        return (
+            <div className="screen">
+                <h1 className="ui heading center">To Do List </h1>
+                <div>
+                    <button onClick={() => {
+                        this.setState({...this.state, taskCount: this.state.taskCount + 1,
+                        });
+                        console.log(this.state.taskCount);
+                    }} className="ui secondary button">Add Task</button>
+                    <p>The number of task are: {this.state.taskCount}</p>
+                </div>
+            </div>
+        );
     }
 }
 
