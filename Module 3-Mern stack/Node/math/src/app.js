a0844459256cd5320bc2d5d4231b1892e912b811
siddhuR <routhusiddhartha@gmail.com>
2023-10-03 18:57:13 +0530

    node modules

--- Module 3-Mern stack/Node/math/src/app.js
+++ Module 3-Mern stack/Node/math/src/app.js
@@ -9,7 +9,7 @@
 // console.log(addNumbers(4, 5, 1, -2, 10, 5));
 
 // ----------------------------------------------
-
+/*
 const http = require("http");
 
 const server = http.createServer((req, res) => {
@@ -18,5 +18,23 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8080, () => {
+  console.log(`Server is running.`);
+});
+*/
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is some response from your first node js server");
+});
+
+app.get("/add", (req, res) => {
+  let { a: firstNumber, b: secondNumber } = req.query;
+  let sum = parseInt(firstNumber) + parseInt(secondNumber);
+  res.send({ sum });
+});
+
+app.listen(8080, () => {
   console.log(`Server is running.`);
 });
