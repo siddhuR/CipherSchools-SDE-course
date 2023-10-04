1c515591c889d0c610fbf6e12a1fd7549e1ef607
siddhuR <routhusiddhartha@gmail.com>
2023-10-04 23:38:06 +0530

    oops.cpp insert till encapsulation and inheritance half

---
+++ Module 1/Codes/Lecture15-onwards-cipherschools/oops.cpp
@@ -0,0 +1,46 @@
+/*
+// Encapsulation
+
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    private:
+    int marks;
+    int attendance;
+    string name;
+
+    public:
+
+    void displaymarks()
+    {
+        cout<<marks;
+    }
+};
+
+int main()
+{
+    int marks;
+    int attendance;
+    string name;
+}
+*/
+
+#include<iostream>
+using namespace std;
+
+class Bus
+{
+    private:
+    
+    int number_tyers;
+    string name;
+    int Capacity;
+    int mileage;
+};
+
+int main()
+{
+
+}
