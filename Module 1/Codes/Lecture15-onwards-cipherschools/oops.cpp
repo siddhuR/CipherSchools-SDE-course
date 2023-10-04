9c0e543eaddf17e9fea3b68cdc0cbea8aedf56e1
siddhuR <routhusiddhartha@gmail.com>
2023-10-05 00:08:31 +0530

    oops.cpp insert till inheritance half

--- Module 1/Codes/Lecture15-onwards-cipherschools/oops.cpp
+++ Module 1/Codes/Lecture15-onwards-cipherschools/oops.cpp
@@ -30,17 +30,60 @@ int main()
 #include<iostream>
 using namespace std;
 
-class Bus
+// The Vehicle is a base class
+
+class Vehicle
 {
-    private:
-    
+    public:
+
     int number_tyers;
     string name;
     int Capacity;
     int mileage;
+
+    public:
+
+    void calculatemileage()
+    {
+        cout<<"The mileage is "<<mileage<<endl;
+    }
+
+};
+
+// Now i will inherit the features of the base class into the derived classes
+
+class SchoolBus: public Vehicle
+{
+    private:
+    string school_name;
+    int numberr_holidays;
+
+    public:
+
+    
+
+    void printnameofschool()
+    {
+        cout<<"The name of the school is "<<name;
+    }
+};
+
+class Car: public Vehicle
+{
+    private:
+
+    int number_airbags;
+    int ac_consumption;
+
+
 };
 
 int main()
 {
+    Car a;
+
+    a.mileage = 50;
+
+    a.calculatemileage();
 
 }
